76dd9a237ff1f3093675362562bc37de80b0d5b5
Justin Clareburt <justcla@microsoft.com>
2018-02-06 16:58:16 -0800

    Now properly hiding the tab bar with Visibility

--- VSWindowManager/Common/GutterService.cs
+++ VSWindowManager/Common/GutterService.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace VSWindowManager
 {
     public class GutterService
     {
-        private const double MARGIN_HIDE_WIDTH = -23;
-
         private Window _window;
 
         public GutterService(Window pWindow)
@@ -20,50 +15,35 @@ namespace VSWindowManager
 
         public void ToggleAllSideBars()
         {
-            // Refresh the side bar list
-            List<FrameworkElement> allSideBars = GetAllSideBars();
+            // Fetch an up-to-date list of gutters
+            List<FrameworkElement> allGutters = GetAllTabGutters();
+            if (allGutters == null) return; // There are no tab gutters to toggle.
 
-            // If any margins contain a negative margin, then we have hidden some hidden gutters.
-            bool haveHiddenGutters = allSideBars.Exists(x => x.Margin.ToString().Contains("-"));
-            bool showGutters = !haveHiddenGutters;
+            // To toggle margins, check if any area already hidden.
+            bool haveHiddenGutters = allGutters.Exists(x => x.Visibility == Visibility.Collapsed);
 
-            // Set the visibility of each side bar
-            foreach(FrameworkElement sideBar in allSideBars)
-            {
-                Thickness newMargin = sideBar.Margin;
-                switch (GetDockPosition(sideBar))
-                {
-                    case Dock.Left:
-                        newMargin.Left = (showGutters ? 0 : MARGIN_HIDE_WIDTH);
-                        break;
-                    case Dock.Right:
-                        newMargin.Right = (showGutters ? 0 : MARGIN_HIDE_WIDTH - 4);    // Set Right margin slightly skinnier
-                        break;
-                    case Dock.Top:
-                        newMargin.Top = (showGutters ? 0 : MARGIN_HIDE_WIDTH);
-                        break;
-                    case Dock.Bottom:
-                        newMargin.Bottom = (showGutters ? 0 : MARGIN_HIDE_WIDTH);
-                        break;
-                    default:
-                        break;
-                }
-                sideBar.Margin = newMargin;
-            }
+            // Set the visibility of each tab bar
+            Visibility newVisibility = (haveHiddenGutters ? Visibility.Visible : Visibility.Collapsed);
+            allGutters.ForEach(x => x.Visibility = newVisibility);
         }
 
-        private List<FrameworkElement> GetAllSideBars()
+        private List<FrameworkElement> GetAllTabGutters()
         {
-            var sideBarObjects = FindChildrenByType(_window, "AutoHideChannelControl");
-            if (sideBarObjects == null || sideBarObjects.Count == 0) return null;
+            List<DependencyObject> autohideChannelControls = FindChildrenByType(_window, "AutoHideChannelControl");
+            if (autohideChannelControls == null || autohideChannelControls.Count == 0) return null;
 
-            List<FrameworkElement> allSideBars = new List<FrameworkElement>();
-            foreach (DependencyObject sideBarObject in sideBarObjects)
+            // Get the "ItemsPresenter" from the Grid inside the AutoHideChannelControl object
+            List<FrameworkElement> allTabGutters = new List<FrameworkElement>();
+            foreach (DependencyObject autoHideChannelControl in autohideChannelControls)
             {
-                allSideBars.Add((FrameworkElement)sideBarObject);
+                // Note: Not doing null checking here. Expecting all AutoHideChannelControls to have an "ItemsPresenter" inside a (Grid).
+                foreach (DependencyObject grid in FindChildrenByType(autoHideChannelControl, "Grid"))
+                {
+                    allTabGutters.Add((FrameworkElement)((FrameworkElement)grid).FindName("ItemsPresenter"));
+                }
             }
 
-            return allSideBars;
+            return allTabGutters;
         }
 
         private static List<DependencyObject> FindChildrenByType(DependencyObject parent, string typeName)
@@ -102,21 +82,6 @@ namespace VSWindowManager
             }
 
             return matches;
-        }
-
-        private static Dock GetDockPosition(FrameworkElement sideBar)
-        {
-            // Use reflection to get the value of ChannelDock from the sideBar (AutoHideChannelControl)
-            // ChannelDock is a DependencyProperty. Must dig into DataContext.
-            object dataContextValue = GetPropertyValue(sideBar, "DataContext");
-            return (Dock)GetPropertyValue(dataContextValue, "Dock");
-        }
-
-        private static object GetPropertyValue(object obj, string propertyName)
-        {
-            Type type = obj.GetType();
-            PropertyInfo propertyInfo = type.GetProperty(propertyName);
-            return propertyInfo.GetValue(obj);
         }
 
     }
